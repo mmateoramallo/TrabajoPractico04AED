3d1bf20e51a85b6c47c0190e13602fa49de0e56e
Mateo <mateo.j.ramallo@gmail.com>
2022-09-30 22:08:37 -0300

    second commit

--- Principal.py
+++ Principal.py
@@ -17,16 +17,11 @@ def mostrar_menu():
     print("8. Salir")
 
 
-# Primer Punto
+#  Cargar: Cargar el contenido del archivo en un vector de registros de proyectos (defina la clase Proyecto), donde cada registro tenga los siguientes campos: nombre_usuario: cadena de caracteres.
 def abrir_archivo():
-    m = open("proyectos.csv", mode="r", encoding="utf8")
+    m = open("proyectos.csv", mode="rt", encoding="utf8")
     filename = "proyectos.csv"
-    c = 0
-    # Mostramos algunos datos de prueba
-    for linea in m:
-        if c == 1:
-            print(linea, end='')
-        c += 1
+    return m
 
 
 def busq_repo(vec, proyecto):
@@ -40,7 +35,7 @@ def busq_repo(vec, proyecto):
 
 def cargar_registros(vec):
     # Abrimos el archivo
-    m = open("proyectos.csv", mode="rt", encoding="utf8")
+    m = abrir_archivo()
     filename = "proyectos.csv"
     c = 0
     regs_cargados = 0
@@ -92,7 +87,100 @@ def add_in_order(vec, proyecto):
     vec[pos:pos] = [proyecto]
 
 
+# Filtrar por tag: Cargar por teclado un tag (cadena de caracteres) y a partir del vector de proyectos, mostrar todos aquellos registros que contengan al tag en alguno de los elementos del vector alojado en el campo tag. Los proyectos deben mostrarse a razón de un registro por línea mostrando solamente el nombre del repositorio, la fecha de actualización y  cantidad de estrellas.
+def validar_tag():
+    tag = input('Ingrese el tag a filtrar por proyectos: ')
+    while tag == "":
+        tag = input('Ingrese el tag a filtrar por proyectos: ')
+
+    return tag
+
+
+def convert_likes(proyecto):
+    likes = proyecto.likes
+    pos_p = None
+    pos_k = None
+    # Recorro el string likes
+    for i in range(len(likes)):
+        if likes[i] == ".":
+            pos_p = i
+        elif likes[i] == "k":
+            pos_k = i
+
+    if "." in proyecto.likes:
+        # Obtengo los numeros delante de la coma
+        nros_adelante = likes[:pos_p]
+        # Obtengo los numeros despues de la coma
+        nros_detras = likes[pos_p:pos_k]
+        # Asumimos que nos quedamos con el indice (1) del string nros_detras, y le agrego dos ceros
+        nros_detras = nros_detras[1]
+        nro_final = nros_adelante + nros_detras + '00'
+        likes = int(nro_final)
+    else:
+        likes_str = (proyecto.likes)
+        # Se tiene que eliminar la K y agregar los tres ceros
+        for a in range(len(likes_str)):
+            if "k" in likes_str:
+                pos_k = a
+                nros_adelante = likes_str[:pos_k]
+                likes_str = nros_adelante + '000'
+                likes = int(likes_str)
+    return likes
+
+
+def cant_estrellas(proyecto):
+    estrellas = 0
+    likes = convert_likes(proyecto)
+    if 0 <= likes <= 10000:
+        estrellas = 1
+    elif 10001 <= likes <= 20000:
+        estrellas = 2
+    elif 20001 <= likes <= 30000:
+        estrellas = 3
+    elif 30001 <= likes <= 40000:
+        estrellas = 4
+    elif likes > 40000:
+        estrellas = 5
+
+    return estrellas
+
+
+def filtrar_tag(vec):
+    # Abrir el archivo
+    m = abrir_archivo()
+    # Solicitamos por teclado el tag al usuario
+    tag = validar_tag()
+    # Ciclo for para buscar proyectos que coincidan con el tag provisto por el usuario
+    for i in vec:
+        if tag in i.tags:
+            # Mostramos la cantidad de estrellas del proyecto segun la cantidad de likes del mismo
+            estrella_proyecto = cant_estrellas(i)
+            # Una vez que determinamos la cantidad de estrellas que tiene1 el proyecto lo mostramos
+            i.show_with_tag(estrella_proyecto)
+
+    # Cerrar el archivo
+    m.close()
+
+
+# Punto 3
+def lenguajes(vec):
+    # Abrir el archivo
+    m = abrir_archivo()
+    # Vector para almacenar los lenguajes de programacion
+    vec_leng = []
+    vec_cont = []
+    # Recorro el vec
+    for v in range(len(vec)):
+        for g in range(len(vec_leng)):
+            # Veo si el lenguaje del v.lenguaje esta en el vec_leng?
+            if vec[v].lenguaje == vec_leng[g]:
+                pass
+            else:
+                vec_leng.append(vec[v].lenguaje)
+
+
 def principal():
+    print('*' * 21, 'Gestión de Proyectos', '*' * 21)
     # Vector
     vec = []
     # Solicitamos una opcion al usuario
@@ -107,12 +195,22 @@ def principal():
     while opcion != 8:
         if opcion == 1:
             vec, regs_cargados, regs_omitidos = cargar_registros(vec)
+            """
             for v in vec:
                 print(v)
-            print(regs_cargados)
-            print(regs_omitidos)
+                print(type(v.likes))
+            """
+            print('-' * 15, '>La cantidad de registros cargados en nuestro vector es:', regs_cargados)
+            print('-' * 15, '>La cantidad de registros omitidos de la carga es:', regs_omitidos)
         elif opcion == 2:
-            pass
+            filtrar_tag(vec)
+            """
+            for v in vec:
+                print(v)
+                lk = (convert_likes(v))
+                # print(lk)
+                # print(type(lk))
+            """
         elif opcion == 3:
             pass
         elif opcion == 4:
